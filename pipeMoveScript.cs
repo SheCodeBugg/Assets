eccafa334983f54bc7854fc028723b19cbfa7597
SheCodeBugg <shelbylbugg@gmail.com>
2025-07-02 00:26:08 -0400

    yep

---
+++ pipeMoveScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class pipeMoveScript : MonoBehaviour
+{
+    [SerializeField] private GameObject pipe;
+    public float spawnRate = 2;
+    private float timer = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawnRate)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            Instantiate(pipe, transform.position, transform.rotation);
+            timer = 0;
+        }
+    }
+}
